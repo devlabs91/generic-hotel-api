f400406add548a9ac2516a91b7919545abd85041
Andy A <anders.a9010@gmail.com>
2023-01-11 19:40:56 +0700

    Added Extension to RateAmountMessages.

--- src/Models/HotelRatePlanNotifRQ/RateAmountMessagesItems.php
+++ src/Models/HotelRatePlanNotifRQ/RateAmountMessagesItems.php
@@ -27,6 +27,9 @@ class RateAmountMessagesItems extends ClassStructure
     
     /** @var RateAmountMessagesItemsAdvancedBookingOffsetItems[]|array */
     public $advancedBookingOffset = NULL;
+
+    /** @var RateAmountMessagesItemsExtensionItems[]|array */
+    public $extension = NULL;
     
     /** @var RateAmountMessagesItemsRatesItems[]|array */
     public $rates = NULL;
@@ -35,13 +38,14 @@ class RateAmountMessagesItems extends ClassStructure
      * @param RateAmountMessagesItemsStatusApplicationControl $statusApplicationControl
      * @param RateAmountMessagesItemsRatesItems[]|array $rates
      */
-    public function __construct(RateAmountMessagesItemsStatusApplicationControl $statusApplicationControl = NULL, $rates = NULL, $restrictionStatus = NULL, $lengthsOfStay = NULL, $advancedBookingOffset = NULL)
+    public function __construct(RateAmountMessagesItemsStatusApplicationControl $statusApplicationControl = NULL, $rates = NULL, $restrictionStatus = NULL, $lengthsOfStay = NULL, $advancedBookingOffset = NULL, $extension = NULL)
     {
         $this->statusApplicationControl = $statusApplicationControl;
         $this->rates = $rates;
         $this->restrictionStatus = $restrictionStatus;
         $this->lengthsOfStay = $lengthsOfStay;
         $this->advancedBookingOffset = $advancedBookingOffset;
+        $this->extension = $extension;
     }
 
     /**
@@ -68,6 +72,10 @@ class RateAmountMessagesItems extends ClassStructure
         $properties->advancedBookingOffset->items = RateAmountMessagesItemsAdvancedBookingOffsetItems::schema();
         $properties->advancedBookingOffset->title = "The Advancedbookingoffset Schema";
         $ownerSchema->addPropertyMapping('AdvancedBookingOffset', self::names()->advancedBookingOffset);
+        $properties->extension = Schema::arr();
+        $properties->extension->items = RateAmountMessagesItemsExtensionItems::schema();
+        $properties->extension->title = "The Extension Schema";
+        $ownerSchema->addPropertyMapping('Extension', self::names()->extension);
         $ownerSchema->type = 'object';
         $ownerSchema->title = "The Items Schema";
         $ownerSchema->required = array(
@@ -338,6 +346,67 @@ class RateAmountMessagesItems extends ClassStructure
     {
         if(!is_array($this->advancedBookingOffset)) { $this->advancedBookingOffset = []; }
         $this->advancedBookingOffset[$key] = $RateAmountMessagesItemsAdvancedBookingOffsetItems;
+        return $this;
+    }
+    /** @codeCoverageIgnoreEnd */
+    
+    /**
+     * @return RateAmountMessagesItemsExtensionItems[]|array
+     * @codeCoverageIgnoreStart
+     */
+    public function getExtension()
+    {
+        return $this->extension;
+    }
+    /** @codeCoverageIgnoreEnd */
+    
+    /**
+     * @param RateAmountMessagesItemsExtensionItems[]|array $extension
+     * @return $this
+     * @codeCoverageIgnoreStart
+     */
+    public function setExtension($extension = NULL)
+    {
+        $this->extension = $extension;
+        return $this;
+    }
+    /** @codeCoverageIgnoreEnd */
+    
+    /**
+     * @param RateAmountMessagesItemsExtensionItems $RateAmountMessagesItemsExtensionItems
+     * @return $this
+     * @codeCoverageIgnoreStart
+     */
+    public function addExtension(RateAmountMessagesItemsExtensionItems $RateAmountMessagesItemsExtensionItems)
+    {
+        if(!is_array($this->extension)) { $this->extension = []; }
+        $this->extension[] = $RateAmountMessagesItemsExtensionItems;
+        return $this;
+    }
+    /** @codeCoverageIgnoreEnd */
+    
+    /**
+     * @param string $key
+     * @return RateAmountMessagesItemsExtensionItems|null
+     * @codeCoverageIgnoreStart
+     */
+    public function getExtensionByKey($key)
+    {
+        if(is_array($this->extension) && key_exists($key, $this->extension) ) { return $this->extension[$key]; }
+        return null;
+    }
+    /** @codeCoverageIgnoreEnd */
+    
+    /**
+     * @param string $key
+     * @param RateAmountMessagesItemsExtensionItems $RateAmountMessagesItemsExtensionItems
+     * @return $this
+     * @codeCoverageIgnoreStart
+     */
+    public function setExtensionByKey($key, RateAmountMessagesItemsExtensionItems $RateAmountMessagesItemsExtensionItems)
+    {
+        if(!is_array($this->extension)) { $this->extension = []; }
+        $this->extension[$key] = $RateAmountMessagesItemsExtensionItems;
         return $this;
     }
     /** @codeCoverageIgnoreEnd */
